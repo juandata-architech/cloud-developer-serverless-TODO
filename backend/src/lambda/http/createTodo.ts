93957d1269437e4c851e671ae7bd83ff4ac9d34a
juandata-architech <juandavid.torres@gmail.com>
2022-10-13 12:30:33 -0500

    createTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,21 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
-
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { createTodo } from '../../businessLogic/createTodo'
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-
-    return undefined
-)
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    return createTodo(event);
+  }
+).use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: false
+    }))
