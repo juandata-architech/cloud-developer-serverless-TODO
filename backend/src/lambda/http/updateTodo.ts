ab3dcf27aa0339002ffb6cb531c41019b6406933
juandata-architech <juandavid.torres@gmail.com>
2022-10-13 12:33:00 -0500

    modified updateTODO

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -3,20 +3,16 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { updateTodo } from '../../businessLogic/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-import { getUserId } from '../utils'
+import { updateTodo } from '../../businessLogic/updateTodo'
+
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
-
-    return undefined
-)
+    return updateTodo(updatedTodo, todoId, updatedTodo.attachmentUrl)
+  })
 
 handler
   .use(httpErrorHandler())
